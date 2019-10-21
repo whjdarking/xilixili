eb404337ad4c11fb4404b9973fa843c4dca24e0a
whjdarking <52321697+whjdarking@users.noreply.github.com>
2019-10-21 16:47:16 +0900

    fix cor

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,10 +12,10 @@ func Cors() gin.HandlerFunc {
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
 	if gin.Mode() == gin.ReleaseMode {
 		// 生产环境需要配置跨域域名，否则403
-		config.AllowOrigins = []string{"http://www.example.com","http://localhost:8080"}
+		config.AllowOrigins = []string{"http://www.xilixili.org","http://localhost:8080"}
 	} else {
 		// 测试环境下模糊匹配本地开头的请求
-		config.AllowOrigins = []string{"http://www.example.com","http://localhost:8080"}
+		config.AllowOrigins = []string{"http://www.xilixili.org","http://localhost:8080"}
 		//config.AllowOriginFunc = func(origin string) bool {
 		//	if regexp.MustCompile(`^http://127\.0\.0\.1:\d+$`).MatchString(origin) {
 		//		fmt.Println(origin)
